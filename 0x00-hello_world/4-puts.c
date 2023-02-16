38773d4781605e8cc748b0f06af720c3218f9d8b
Coding-Ga <benjikofi23@gmail.com>
2023-02-16 13:56:10 -0800

    Task 4 commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main - This is my main function
+ *
+ * Return: always gives 0
+ */
+
+int main(void)
+{
+      puts("\"Programming is like building a multilingual puzzle");
+      return (0);
+}
