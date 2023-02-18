bca506f9529dbe4cf3e99ac81f2eda007b26e6c7
Coding-Ga <benjikofi23@gmail.com>
2023-02-17 20:02:33 -0800

    Task 9 commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,1 +1,25 @@
 #include <stdio.h>
+#include <unistd.h>
+/**
+ * main - This is the main function
+ * Description: prints or outputs all possible single-digit numbers
+ * Return: Gives a value of 0
+ */
+
+int main(void)
+{
+int i;
+for (i = '0'; i <= '9' ; i++)
+{
+putchar(c);
+
+if (i != '9')
+{
+putchar(',');
+putchar(' ');
+}
+
+}
+putchar('\n');
+return (0);
+}
