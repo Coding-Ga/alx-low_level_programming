88bdf440fc39bea84ed0326dad93b9d68803570b
Coding-Ga <benjikofi23@gmail.com>
2023-02-17 17:39:09 -0800

    Task 2 commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - This is the main function
+ * Description: prints or outputs the alphabet in lowercase\n
+ * Return: Gives a value of 0
+ */
+
+int main(void)
+{
+int ch;
+
+for (ch = a; ch <= z; ch++)
+{
+putchar(ch);
+}
+putchar(n);
+return (0);
+}
