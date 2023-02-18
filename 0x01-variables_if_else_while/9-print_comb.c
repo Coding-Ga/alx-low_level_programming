647a653145730d091125be611dc300e6cc8481c7
Coding-Ga <benjikofi23@gmail.com>
2023-02-17 19:50:13 -0800

    Task 9 commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - This is the main function
+ * Description: prints or outputs all possible combs of single-digit numbers
+ * Return: Gives a value of 0
+ */
+
+int main(void)
+{
+int i;
+
+for (i = '0'; i <= '9' ; i++)
+{
+putchar(c);
+
+if (i != '9')
+{
+putchar(',');
+putchar(' ');
+}
+
+}
+return (0);
+}
---
+++ 0x01-variables_if_else_while/be.c
@@ -0,0 +1,1 @@
+ * Description: prints or outputs all possible combs of single-digit numbers
