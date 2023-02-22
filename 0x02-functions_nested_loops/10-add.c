50b36455d7cb50d26151a51e26ec69de0634c9c8
Coding-Ga <benjikofi23@gmail.com>
2023-02-22 12:00:35 -0800

    Task 10 commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Add function 
+ * @a: int to be added to b integer
+ * @b: int to be added to a integer
+ * Return: the value of the sum of a and b
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
