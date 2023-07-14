1137e0c5c936642b98485b75abd5d9d517330f0e
Coding-Ga <benjikofi23@gmail.com>
2023-07-13 18:52:57 -0700

    Commitme

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 
 /**
- *main -  A script that generates the assembly code (Intel syntax) 
+ *main -  A script that generates the assembly code (Intel syntax)
  *
  * of a C code and save it in an output file.
  *
