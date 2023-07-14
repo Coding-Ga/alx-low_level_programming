168f73724610f6066c5e9cdb1e6e71a2b0bb10d0
Coding-Ga <benjikofi23@gmail.com>
2023-07-13 18:47:32 -0700

    Second commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ *main -  A script that generates the assembly code (Intel syntax) 
+ *
+ * of a C code and save it in an output file.
+ *
+ *Return: 1
+ */
+
+
+int main(void)
+
+
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar,"
+
+	" 2015-10-19\n");
+
+	return (1);
+}
