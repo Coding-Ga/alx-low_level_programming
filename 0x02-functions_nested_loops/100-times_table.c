9e48d3248605d40d46ca331abe94e8c3f9d1c21f
Coding-Ga <benjikofi23@gmail.com>
2023-07-18 18:41:10 -0700

    Commiting v

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,37 +1,14 @@
 #include <stdio.h>
 #include "main.h"
 
-/**
-  * print_times_table - Print time tables as specified
-  * @n: The number of times the table would be printed
+/**                                                                               
+  * print_times_table - Print time tables as specified                            
+  * @n: The number of times the table would be printed                            
   * Returns: 0 (success)
   */
 
-void print_times_table(int n)
+int main(int n)
 {
-	int i;
-	int j;
+    int num, mult, prod;
 
-	if (n > 15 || n < 0)
-	{
-		return;
-	}
-	else
-	{
-		for (i = 0; i <= n; i++)
-		{
-			for (j = 0; j <= n; j++)
-			{
-				int sum = i * j;
-
-				printf("%d", sum);
-
-				if (j < n)
-				{
-					printf(", ");
-				}
-			}
-			printf("\n");
-		}
-	}
 }
